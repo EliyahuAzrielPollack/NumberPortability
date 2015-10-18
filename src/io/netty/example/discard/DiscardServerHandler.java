d43bb725db1f05e68e7c8fbd9c507f5c6bd52c28
eli <eli020187@gmail.com>
2015-10-18 16:16:45 +0200

    discard server works well.

--- src/io/netty/example/discard/DiscardServerHandler.java
+++ src/io/netty/example/discard/DiscardServerHandler.java
@@ -3,6 +3,7 @@ package io.netty.example.discard;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
@@ -11,8 +12,20 @@ public class DiscardServerHandler extends ChannelHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { 
-        // Discard the received data silently.
-        ((ByteBuf) msg).release(); 
+    	
+    	// Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    	
+        // Print the message on the console.
+    	/*ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }*/
     }
 
     @Override
